533118a6644ef8e23be879a6575de6adc55d7140
SilverWithA <SilverWithA@gamil.com>
2024-01-08 21:20:11 +0900

    feat: build puuid class

--- dags/custom_modules/model/matchIDCollector.py
+++ dags/custom_modules/model/matchIDCollector.py
@@ -1,11 +1,30 @@
-class MatchID():
-    def __init__(self):
-        self.matchIDs = []
+from dags.custom_modules.constant.tiers import *
+from dags.custom_modules.constant.APIkey import *
+import requests
 
-    def request_matchId(self, tier, puuid):
+class SummonerName():
+    def requeset_summonerNames(tier):
+        summonerNames = []
+
         """request summoner API and collect summoner Names in specific tier"""
-        pass
+        summoner_url = tier.summoner_url + str(APIkey.PRODUCION_KEY.str_key)
+        summoner_r = requests.get(summoner_url)
 
-    def collect_matchIDs(self, puuids):
-        """collect specific tier's matchIDs unsing by multi-thread"""
-        pass
+        summonerName_count = len(summoner_r.json()["entries"])
+
+        for i in range(summonerName_count):
+            summonerNames.append(summoner_r.json()["entries"][i]["summonerName"])
+
+        return summonerNames
+
+
+
+# 사용 예시
+# print(SummonerName.requeset_summonerNames(Tier.CHALLENGER)[:10])  # 챌린저
+# print(SummonerName.requeset_summonerNames(Tier.GRANDMASTER)[:10])  # 그마
+# print(SummonerName.requeset_summonerNames(Tier.MASTER)[:10])  # 마스터
+
+# ------------- 한 페이지에 대략 200명
+u = "https://kr.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page=1&api_key=RGAPI-82d303c3-356f-4cbe-83b6-6ac2ca16567c"
+r = requests.get(u)
+print(len(r.json()))
--- dags/custom_modules/model/puuidCollector.py
+++ dags/custom_modules/model/puuidCollector.py
@@ -1,11 +1,42 @@
-class Puuid():
+import threading
+import requests
+import time
 
+from dags.custom_modules.constant.APIkey import *
+
+
+class Puuid():
     def __init__(self):
         self.puuids = []
-    def request_puuid(self, tier, summonerName):
+    def _request_puuid(self, summonerName):
         """request puuid API and collect summoner Names in specific tier"""
-        pass
+        result_data_lock = threading.Lock()
+        try:
+            puuid_url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + str(
+                summonerName) + "?api_key=" + APIkey.PRODUCION_KEY.str_key
+            puuid = requests.get(puuid_url).json()["puuid"]
+
+            with result_data_lock:
+                self.puuids.append(puuid)
+            return
+        except Exception as e:
+            return
 
     def collect_puuids(self, summonerNames):
         """collect specific tier's puuids unsing by multi-thread"""
-        pass
+        puuid_threads = []
+        for i in range(len(summonerNames)):
+
+            if i % 50 == 0 and i > 0:
+                time.sleep(10)
+
+            thread = threading.Thread(target=_request_puuid, args=(summonerNames[i],))
+            puuid_threads.append(thread)
+            thread.start()
+
+        # 모든 스레드가 종료될 때까지 대기
+        for thread in puuid_threads:
+            thread.join()
+
+        print("수집한 puuid 개수: ", len(self.puuids))
+        return self.puuids
