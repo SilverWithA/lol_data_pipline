f4c46f50a2e37771a00f56cac74d9b1024426df7
SilverWithA <SilverWithA@gamil.com>
2024-01-09 12:28:20 +0900

    feat: make summoner collecting flow in diamond

--- dags/custom_modules/constant/tiers.py
+++ dags/custom_modules/constant/tiers.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 class Tier(Enum):
+    DIVISIONS = (["I","II","III","IV"])
     CHALLENGER = ("https://kr.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=")
     GRANDMASTER = ("https://kr.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5?api_key=")
     MASTER = ("https://kr.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key=")
--- dags/custom_modules/model/matchIDCollector.py
+++ dags/custom_modules/model/matchIDCollector.py
@@ -1,47 +1,47 @@
-from dags.custom_modules.constant.tiers import *
-from dags.custom_modules.constant.APIkey import *
+from custom_modules.constant.tiers import *
+from custom_modules.constant.APIkey import *
 import requests
 
 class SummonerName():
     def requeset_summonerNames(tier):
         """request summoner API and collect summoner Names in specific tier"""
         summonerNames = []
-        if tier not in [Tier.CHALLENGER, Tier.GRANDMASTER, Tier.MASTER]:
-            for division in ["I","II","III","IV"]:
-                summoner_url = tier.summoner_url + division + "?page=1&api_key=" + str(APIkey.PRODUCTION_KEY.str_key)
+        errors = []
+        try:
+            if tier in [Tier.CHALLENGER, Tier.GRANDMASTER, Tier.MASTER]:
+                summoner_url = tier.summoner_url + str(APIkey.PRODUCTION_KEY.str_key)
                 summoner_r = requests.get(summoner_url)
 
-                summonerName_count = len(summoner_r.json())
+                summonerName_count = len(summoner_r.json()["entries"])
 
                 for i in range(summonerName_count):
-                    summonerNames.append(summoner_r.json()[i]['summonerName'])
+                    summonerNames.append(summoner_r.json()["entries"][i]["summonerName"])
 
-            return summonerNames
-        else:
-            summoner_url = tier.summoner_url + str(APIkey.PRODUCTION_KEY.str_key)
-            summoner_r = requests.get(summoner_url)
+                return summonerNames
+            # under master ranking = diamond ~ bronze
+            else:
+                for division in Tier.DIVISIONS.summoner_url:
+                    print(division)
+                    summoner_url = tier.summoner_url + division + "?page=1&api_key=" + str(APIkey.PRODUCTION_KEY.str_key)
+                    summoner_r = requests.get(summoner_url)
 
-            summonerName_count = len(summoner_r.json()["entries"])
+                    summonerName_count = len(summoner_r.json())
 
-            for i in range(summonerName_count):
-                summonerNames.append(summoner_r.json()["entries"][i]["summonerName"])
+                    for i in range(summonerName_count):
+                        summonerNames.append(summoner_r.json()[i]['summonerName'])
 
-            return summonerNames
+                ############################
+                print(len(summonerNames))
+                return summonerNames
+
+        except Exception as e:
+            errors.append(e)
 
 
 
 
-# 사용 예시
+# 사용 예시 및 테스트 코드
 # print(SummonerName.requeset_summonerNames(Tier.CHALLENGER)[:10])  # 챌린저
 # print(SummonerName.requeset_summonerNames(Tier.GRANDMASTER)[:10])  # 그마
 # print(SummonerName.requeset_summonerNames(Tier.MASTER)[:10])  # 마스터
 # print(len(SummonerName.requeset_summonerNames(Tier.DIAMOND))) # 다이아 이하
-
-
-# ------------- 한 페이지에 대략 200명
-# u = "https://kr.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page=1&api_key=RGAPI-82d303c3-356f-4cbe-83b6-6ac2ca16567c"
-# r = requests.get(u)
-# for i in range(10):
-#     print(r.json()[i]['summonerName'])
-
-# print(len(SummonerName.requeset_summonerNames(Tier.DIAMOND)))
